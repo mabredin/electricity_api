960034db0ec934d649cbf42d86d217f8dbebab3c
mabredin <mbredin@bk.ru>
2023-11-07 15:30:33 +0300

    Add rest_framework in INSTALLED_APPS

--- electricity/electricity/settings.py
+++ electricity/electricity/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'mainsite.apps.MainsiteConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
